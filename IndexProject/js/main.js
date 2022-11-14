51761a56a0646b646a0b1b7a553b57d2f79fcd1e
root4554 <anass.kbir@hotmail.com>
2022-11-14 08:18:56 +0100

    add changes to idex

--- IndexProject/js/main.js
+++ IndexProject/js/main.js
@@ -3,10 +3,14 @@ const fZoom = () => {
   document.querySelector("#myName").classList.add("bigger");
 };
 
-const fhide = () => {
+/******* HIDE ENTRANCE ***********/
+//toggle the visibility between entrance and intro
+const toggleviews = () => {
   document.querySelector(".entrance").style.display = "none";
+  document.querySelector(".intro").style.display = "block";
+  document.querySelector(".about").style.display = "block";
 };
-setTimeout(fhide, 3700);
+setTimeout(toggleviews, 3700);
 window.onload = fZoom;
 
 /******* BALLS ANIMATION ********** */
@@ -33,10 +37,82 @@ function changePosition(event) {
   balls.style.setProperty("--xB", getRandomNumber(0, 90) + "vw");
   balls.style.setProperty("--yB", getRandomNumber(0, 90) + "vh");
 
-  console.log(balls);
+  // console.log(balls);
 }
 
 function getRandomNumber(low, high) {
   let r = Math.floor(Math.random() * (high - low + 1)) + low;
   return r;
 }
+
+/******* ZOOMIN TO ABOUT CONTENT EFFECT ********** */
+let body = document.querySelector("body");
+let about = document.querySelector(".about");
+
+const createHeight = () => {
+  let AboutHeight = about.offsetHeight;
+  // console.log("height " + AboutHeight);
+  let windowHeight = window.innerHeight;
+  // console.log("body " + windowHeight);
+  let newHeight = windowHeight * 2;
+  // console.log("new " + newHeight);
+  body.style.height = newHeight + "px";
+};
+
+setTimeout(createHeight, 3700);
+
+const zoomOnScroll = () => {
+  let scroll = window.scrollY;
+
+  let zoom = 1 + scroll / 5;
+  let zoomMax = Math.min(zoom, 100);
+
+  let roll = (scroll * 360) / window.innerHeight;
+  let opacity = (scroll * 2) / window.innerHeight;
+
+  let width = (scroll * 100) / window.innerHeight;
+  let widthMax = Math.min(width, 100);
+
+  let height = (scroll * 100) / window.innerHeight;
+  let heightMax = Math.min(height, 100);
+
+  // let removeRadius = (scroll - 1) / 5;
+  // console.log(removeRadius);
+
+  document.querySelector(".presentingText").style.transform = `scale(${zoom})`;
+
+  about.style.transform = `rotate(${roll}deg) scale(${zoomMax})`;
+  about.style.opacity = `${opacity}`;
+  about.style.width = `${widthMax}%`;
+  about.style.height = `${heightMax}vh`;
+  // about.style.borderRadius = `${removeRadius}%`;
+};
+
+window.addEventListener("scroll", zoomOnScroll);
+
+/******* START EFFECT OF ABOUT CONTENT ********** */
+
+const startEffect = () => {
+  let scroll = window.scrollY;
+  let windowHeight = window.innerHeight;
+  let aboutHeight = about.offsetHeight;
+  let aboutTop = about.offsetTop;
+  let aboutBottom = aboutTop + aboutHeight;
+
+  const conts = document.querySelectorAll(".conts");
+  const afters = window.getComputedStyle(conts, "::after");
+
+  // if (scroll > aboutTop - windowHeight && scroll < aboutBottom) {
+  if (scroll > windowHeight) {
+    conts.forEach((cont) => {
+      cont.classList.add("animation");
+    });
+    // afters.forEach((after) => {
+    //   after.classList.add("animation");
+    // });
+  } else {
+    about.classList.remove("animation");
+  }
+};
+
+window.addEventListener("scroll", startEffect);
